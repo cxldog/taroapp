338987a6ed7cf31369cc9870011f65ed50c81197
cxl1994 <614820769@qq.com>
2020-04-03 14:55:38 +0800

    ceshi

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -22,7 +22,7 @@ export default class Index extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config = {
-    navigationBarTitleText: '首页1',
+    navigationBarTitleText: '首页2',
   }
   state: {
     btn:[
